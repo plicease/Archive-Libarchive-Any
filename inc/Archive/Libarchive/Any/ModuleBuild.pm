1a9622518444488051af818fec17ea3d2404b21d
Graham Ollis <perl@wdlabs.com>
2013-11-21 11:24:52 -0500

    fix for #1

--- inc/Archive/Libarchive/Any/ModuleBuild.pm
+++ inc/Archive/Libarchive/Any/ModuleBuild.pm
@@ -4,4 +4,20 @@ use strict;
 use warnings;
 use base qw( Module::Build );
 
+sub new
+{
+  my($class, %args) = @_;
+  
+  if(eval { require Archive::Libarchive::FFI; 1 })
+  {
+    $args{requires}->{'Archive::Libarchive::FFI'} = 0;
+  }
+  else
+  {
+    $args{requires}->{'Archive::Libarchive::XS'} = 0;
+  }
+  
+  $class->SUPER::new(%args);
+}
+
 1;
